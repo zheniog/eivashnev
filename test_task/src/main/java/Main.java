f461f49af5a6600cae6f5698e6b06fc36accf851
zheniog <zheniog@yandex.ru>
2017-11-11 22:46:31 +0300

    Added log4j support

---
+++ test_task/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Test log");
+
+    }
+}
